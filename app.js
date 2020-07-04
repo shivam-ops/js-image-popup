07145923ad90b78014500c5bdbf8af8660831cf7
Shivam Joshi <shivamjoshi0822@gmail.com>
2020-07-04 10:58:45 +0530

    adding app.js and some correction in css

---
+++ app.js
@@ -0,0 +1,19 @@
+const modal = document.querySelector(".modal");
+const previews = document.querySelectorAll(".gallery img");
+const original = document.querySelector(".full-img");
+const imgText = document.querySelector(".caption");
+
+previews.forEach(preview => {
+    preview.addEventListener("click", () => {
+        modal.classList.add("open");
+        original.classList.add("open");
+    });
+});
+
+
+modal.addEventListener('click', (e) => {
+    if(e.target.classList.contains("modal")) {
+        modal.classList.remove("open");
+        original.classList.remove("open");
+    }
+});
